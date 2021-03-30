9b4cd5d6336b5b38a40fdb2d87a2a06774207bb8
unknown <gustavogst01@gmail.com>
2021-03-30 19:24:34 -0300

    Exercicios resolvidos vanilla javascript

---
+++ quinzena3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,71 @@
+/* 1- Exercicio
+No primeiro console.log aparecerá: 10
+No segundo aparecerá 10 e 5 */
+
+
+/* 2- Exercicio
+Irá aparecer 10, 10 e 10 */
+
+
+/*1. d)
+   acredito que isso aconteceu
+   pelo fato de que eu não atribui nem um 
+   valor para as duas variaveis */
+
+/* e) 
+Aparece que está undefined, undefined
+*/
+
+
+/* let nomeD = console.log('Qual seu nome?')
+let idadeD = console.log('Quantos anos você tem?')
+
+console.log(typeof nomeD, idadeD);
+
+
+let nomeF = "luis andrade";
+let idadeF = 21;
+
+console.log('olá, ' + nomeF + ' você tem ' + idadeF + ' anos' ) */
+
+
+/*2- Exercicio 
+const endereco = console.log("1. Qual o seu endereço?");
+let = respostaEnd = console.log("Resposta Rua ABC, 123")
+
+const cor = console.log("2. Qual sua cor favorita?");
+let = respostaCor = console.log("Resposta: Preto")
+
+const prato = console.log("3. Qual o seu prato favorito?");
+let = respostaEND = console.log("Resposta: pizza")
+
+const musica = console.log("4. Qual o seu estilo de musica?");
+let = respostaEND = console.log("Resposta: metal")
+
+const instrumento = console.log("5. Qual o seu instrumento?");
+let = respostaEND = console.log("Resposta: Guitar") */
+
+
+/* 3- Exercicio 
+
+let comidas = ['Arroz', 'Macarrão', 'Carne','Peixe', 'Salmão']
+console.log(comidas);
+
+console.log('Estas são minhas comidas preferidas: ')
+console.log(comidas [0])
+console.log(comidas [1])
+console.log(comidas [2])
+console.log(comidas [3])
+console.log(comidas [4])
+
+console.log('Qual sua comida preferida?')
+comidas[1] = 'Churrasco';
+console.log(comidas)  */
+
+/* 4- Exercicio  
+const perguntas = ['Você gosta de arroz?', 'Você está usando tenis?', 'Tem um rato na sua mesa?']
+let boolean = ['true' , 'false', 'false'];
+
+console.log(perguntas[0], boolean[0])
+console.log(perguntas[1], boolean[1])
+console.log(perguntas[2], boolean[2]) */
