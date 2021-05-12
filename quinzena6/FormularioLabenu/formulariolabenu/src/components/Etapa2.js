ddef6dfec760aa08889dd71390560f4e03ad5dee
unknown <gustavogst01@gmail.com>
2021-05-11 22:13:35 -0300

    Todas as atividades feitas

---
+++ quinzena6/FormularioLabenu/formulariolabenu/src/components/Etapa2.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+
+
+export class Etapa2 extends React.Component {
+  
+  render(){
+    return (
+        <div className= "App">
+        <h1>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR </h1>
+        <p>5. Qual curso?</p>
+        <input type="text"></input>
+
+        <p>6. Qual a unidade de ensino?</p>
+        <input type="text"></input>
+        
+     </div>
+
+    )
+  }
+
+} 
+
+
+export default Etapa2 ;
